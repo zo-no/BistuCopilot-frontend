bc115068278fc226fa626df76c534bf3a0fb0e5a
zo-no <2742160682@qq.com>
2023-11-02 23:11:59 +0800

    文章管理开发中

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -41,37 +41,44 @@ const Publish = () => {
             ]}
           ></Breadcrumb>
         }
-      ></Card>
-      <Form.Item
-        label="标题"
-        name="title"
-        rules={[{ required: true, message: "请输入文章标题" }]}
       >
-        <Input placeholder="请输入文章标题" style={{ width: 400 }} />
-      </Form.Item>
-      <Form.Item
-        label="频道"
-        name="chanel_id"
-        rules={[{ required: true, message: "请选择文章频道" }]}
-      >
-        <Select placeholder="请选择文章频道" style={{ width: 400 }}>
-          {/* value属性用户选中之后会自动收集起来作为接口的提交字段 */}
-          {/* {channelList.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)} */}
-        </Select>
-      </Form.Item>
-      <Form.Item label="封面">
-        <Form.Item label="type">
-          <Radio.Group>
-            <Radio value={1}>单图</Radio>
-            <Radio value={2}>三图</Radio>
-            <Radio value={3}>无图</Radio>
-          </Radio.Group>
-        </Form.Item>
-        {/* 
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ type: 0 }}
+          onFinish={onFinish}
+          //   form={form}
+        >
+          <Form.Item
+            label="标题"
+            name="title"
+            rules={[{ required: true, message: "请输入文章标题" }]}
+          >
+            <Input placeholder="请输入文章标题" style={{ width: 400 }} />
+          </Form.Item>
+          <Form.Item
+            label="频道"
+            name="chanel_id"
+            rules={[{ required: true, message: "请选择文章频道" }]}
+          >
+            <Select placeholder="请选择文章频道" style={{ width: 400 }}>
+              {/* value属性用户选中之后会自动收集起来作为接口的提交字段 */}
+              {/* {channelList.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)} */}
+            </Select>
+          </Form.Item>
+          <Form.Item label="封面">
+            <Form.Item label="type">
+              <Radio.Group>
+                <Radio value={1}>单图</Radio>
+                <Radio value={2}>三图</Radio>
+                <Radio value={3}>无图</Radio>
+              </Radio.Group>
+            </Form.Item>
+            {/* 
             listType: 决定选择文件框的外观样式
             showUploadList: 控制显示上传列表
         */}
-        {/* {imageType > 0 && <Upload
+            {/* {imageType > 0 && <Upload
               listType="picture-card"
               showUploadList
               action={'http://geek.itheima.net/v1_0/upload'}
@@ -84,11 +91,11 @@ const Publish = () => {
                 <PlusOutlined />
               </div>
             </Upload>} */}
-      </Form.Item>
-      <Form.Item
+          </Form.Item>
+          <Form.Item
             label="内容"
             name="content"
-            rules={[{ required: true, message: '请输入文章内容' }]}
+            rules={[{ required: true, message: "请输入文章内容" }]}
           >
             {/* 富文本编辑器 */}
             {/* <ReactQuill
@@ -105,7 +112,8 @@ const Publish = () => {
               </Button>
             </Space>
           </Form.Item>
-
+        </Form>
+      </Card>
     </div>
   );
 };
