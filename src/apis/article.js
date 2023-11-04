232a6c5aa472773c093cc0050cacf59ba6f4db90
zo-no <2742160682@qq.com>
2023-11-05 00:37:36 +0800

    小改动

---
+++ src/apis/article.js
@@ -0,0 +1,16 @@
+/*
+@Date		:2023/11/04 21:10:19
+@Author		:zono
+@Description: 文章有关接口函数
+*/
+import {request} from "@/utils";
+
+export function getChannalAPI (){
+    /*
+    @Description:一个获取频道的接口
+    */
+    return request({
+        url: '/channels',
+        method: 'GET',
+      });
+}
